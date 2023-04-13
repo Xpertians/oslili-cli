4fe3579402f234dbe70ad7d1826abb3ecc812ac5
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-13 12:03:35 -0700

    fixing version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="oslili-cli", version="0.2",
-    description="oslili-cli",
+    name="oslili-cli", version="0.3",
+    description="Open Source License Identification Library",
     author="Oscar Valenzuela",
     author_email="oscar.valenzuela.b@gmail.com",
     packages=['src'],
