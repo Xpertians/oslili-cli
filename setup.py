e03f2a59e7668d6ebc1fd66595b6450147d07f40
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-20 15:06:25 +0200

    test

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
+import pathlib
+import sys
 from setuptools import setup, find_packages
 
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '0.6'
+PACKAGE_NAME = 'oslili-cli'
+AUTHOR = 'Oscar Valenzuela B.'
+AUTHOR_EMAIL = 'oscar.valenzuela.b@gmail.com'
+URL = 'https://github.com/oscarvalenzuelab/oslili-cli'
+LICENSE = 'Apache-2.0'
+DESCRIPTION = 'Open Source License Identification Library'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
 setup(
-    name="oslili-cli", version="0.4",
-    description="Open Source License Identification Library",
-    author="Oscar Valenzuela",
-    author_email="oscar.valenzuela.b@gmail.com",
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
     packages=['src'],
     entry_points={"console_scripts": ["oslili-cli=src.cli:main"]},
     install_requires=["oslili", ],
-    url='https://opensourcelicensecompliance.com',
+    url=URL,
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: POSIX :: Linux',
-        'Development Status :: 4 - Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License'
     ],
 )
